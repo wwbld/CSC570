47ab458d17e8defc6095d899e2065aec43561eea
Administrator <ywang30@ubuntu.ubuntu-domain>
2018-02-12 14:44:40 -0800

    add visualization

--- Keras/simple_example/example_nn.py
+++ Keras/simple_example/example_nn.py
@@ -3,8 +3,9 @@ os.environ['KERAS_BACKEND'] = 'tensorflow'
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Reshape
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization, Activation
 from keras import backend as K
+import matplotlib.pyplot as plt
 import util
 
 TRAIN = 'iris_training.csv'
@@ -12,7 +13,7 @@ TEST = 'iris_test.csv'
 
 batch_size = 20
 num_classes = 3
-epochs = 2000
+epochs = 60
 
 training_data, training_target = util.read_csv(TRAIN)
 testing_data, testing_target = util.read_csv(TEST)
@@ -21,23 +22,52 @@ training_target = keras.utils.to_categorical(training_target, num_classes)
 testing_target = keras.utils.to_categorical(testing_target, num_classes)
 
 model = Sequential()
+
 model.add(Reshape((2,2,1), input_shape=(4,)))
-model.add(Conv2D(32, kernel_size=(2,2),
-                 activation='relu'))
+model.add(Conv2D(32, kernel_size=(2,2)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
 model.add(Flatten())
-model.add(Dense(7, activation='softmax'))
-model.add(Dropout(0.5))
-model.add(Dense(3, activation='softmax'))
+
+model.add(Dense(7))
+model.add(BatchNormalization())
+model.add(Activation('softmax'))
+
+model.add(Dense(3))
+model.add(BatchNormalization())
+model.add(Activation('softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
               optimizer=keras.optimizers.Adam(),
               metrics=['accuracy'])
 
-model.fit(training_data, training_target,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=2,
-          validation_data=(testing_data, testing_target))
+history = model.fit(training_data, training_target,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=2,
+                    validation_data=(testing_data, testing_target))
 score = model.evaluate(testing_data, testing_target, verbose=0)
 print('Test loss: ', score[0])
 print('Test accuracy: ', score[1])
+
+model.save('model_1')
+
+print(history.history.keys())
+
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title("model accuracy")
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title("model loss")
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
